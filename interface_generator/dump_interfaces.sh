d9559a8ad2f1694ab06c337c5f8dad3d05610de6
Thierry Martinez <thierry.martinez@inria.fr>
2018-08-24 17:45:42 +0200

    Support for OCaml 4.08.0

--- interface_generator/dump_interfaces.sh
+++ interface_generator/dump_interfaces.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-    #3.07 3.08.4 3.09.3 3.10.2 3.11.2 3.12.1 4.00.1 \
-    #4.01.0 4.02.3 4.03.0 4.04.2 4.05.0 4.06.1\
-for ocaml_version in \
-    3.11.2 3.12.1 4.00.1 \
-    4.01.0 4.02.3 4.03.0 4.04.2 4.05.0 4.06.1 \
-    4.07.0+rc1; do
+    #3.11.2 3.12.1 4.00.1 \
+    #4.01.0 4.02.3 4.03.0 4.04.2 4.05.0 4.06.1 \
+    #4.07.0
+for ocaml_version in 4.08.0+trunk; do
   target_dir=../interfaces/${ocaml_version:0:4}
   mkdir -p $target_dir
   for module in \
@@ -18,6 +16,6 @@ for ocaml_version in \
     target=$target_dir/`echo ${module:0:1} | tr A-Z a-z`${module:1}.mli
     opam config exec --switch=$ocaml_version -- \
       ./interface_dumper $module ocaml >$target
-    [ `stat -f"%z" $target` -gt 1 ] || rm $target
+    [ `stat --format="%s" $target` -gt 1 ] || rm $target
   done
 done
--- interface_generator/generate_interfaces.sh
+++ interface_generator/generate_interfaces.sh
@@ -5,10 +5,10 @@ for module in \
     Pervasives Arg Array ArrayLabels Buffer Bytes BytesLabels Callback Char \
     Complex Digest Ephemeron Filename Float Format Gc Genlex Hashtbl Int32 \
     Int64 Lazy Lexing List ListLabels Map Marshal MoreLabels Nativeint Obj Oo \
-    Parsing Printexc Printf Queue Random Scanf Seq Set Sort Spacetime Stack \
+    Parsing Printexc Printf Queue Random Scanf Seq Set Spacetime Stack \
     StdLabels Stream String StringLabels Sys Uchar Weak; do
     target=../stdcompat__`echo $module | tr A-Z a-z`.mli.in
     echo $module
-    ./interface_generator $module 4.07 4.06 4.05 4.04 4.03 4.02 4.01 4.00 3.12 \
+    ./interface_generator $module 4.08 4.07 4.06 4.05 4.04 4.03 4.02 4.01 4.00 3.12 \
        3.11 3.10 3.09 3.08 3.07 >$target
 done
